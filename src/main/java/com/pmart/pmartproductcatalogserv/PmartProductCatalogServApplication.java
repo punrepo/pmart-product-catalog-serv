dfd742ef7f44b9d909b2a6957dcddba5d10f6531
Puneet Pant <pmpant16@gmail.com>
2025-07-30 20:58:55 -0700

    initial commit

---
+++ src/main/java/com/pmart/pmartproductcatalogserv/PmartProductCatalogServApplication.java
@@ -0,0 +1,13 @@
+package com.pmart.pmartproductcatalogserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmartProductCatalogServApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmartProductCatalogServApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pmart/pmartproductcatalogserv/PmartProductCatalogServApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmart.pmartproductcatalogserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmartProductCatalogServApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
